0325466d344a8ab6bbede87e9d08aa06f0b2fe11
Lonechef <saumyaparikh42@gmail.com>
2023-08-27 16:00:42 +0530

    file add

---
+++ client/src/services/api.js
@@ -0,0 +1,30 @@
+// import axios from 'axios';
+
+// const API_URI = 'http://localhost:8000'
+
+// const API_GMAIL = async (urlObject, payload, type) => {
+   
+//     return await axios({
+//         method: urlObject.method,
+//         url: `${API_URI}/${urlObject.endpoint}/${type}`,
+//         data: payload
+//     })
+// }
+
+// export default API_GMAIL;
+
+import axios from 'axios';
+
+const API_URI = ''
+
+const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
+    const { params, urlParams, ...body } = requestData;
+
+    return await axios({
+        method: serviceUrlObject.method,
+        url: `${API_URI}/${serviceUrlObject.endpoint}/${type}`,
+        data: requestData
+    })
+}
+
+export default API_GMAIL;
